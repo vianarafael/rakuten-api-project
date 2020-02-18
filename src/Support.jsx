e7e5bf94b16f09931e8d039c727abfa24e6cc43b
Ashley Kekona <56838258+akekona8@users.noreply.github.com>
2020-02-18 16:16:41 +0900

    commit before merge

--- src/Support.jsx
+++ src/Support.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import "./style/App.css";
 
 function Support() {
-
-  return (
-    <div className="Support">
-      support page~~~
-
-
-    </div>
-  );
+  return <div className="Support">support page~~~</div>;
 }
 
 export default Support;
--- src/redux.js
+++ src/redux.js
@@ -1,13 +1,28 @@
 import { createStore } from "redux";
 
 const initialState = {
-  view: "top",
+  view: "top"
 };
 
-export const changeView = (view) => {
+export const changeView = view => {
   return {
     type: "CHANGE_VIEW",
     view
+  };
+};
+
+export const setDate = date => {
+  return {
+    type: "SET_DATE",
+    date
+  };
+};
+
+export const setLocation = (prefecture, city) => {
+  return {
+    type: "SET_LOCATION",
+    prefecture,
+    city
   };
 };
 
@@ -15,7 +30,17 @@ const reducer = (state = initialState, action) => {
   // eslint-disable-next-line default-case
   switch (action.type) {
     case "CHANGE_VIEW": {
-      return { ...state, view: action.view};
+      return { ...state, view: action.view };
+    }
+    case "SET_DATE": {
+      return { ...state, date: action.date };
+    }
+    case "SET_LOCATION": {
+      const newLocation = {
+        prefecture: action.prefecture,
+        city: action.city
+      };
+      return { ...state, newLocation };
     }
   }
   return state;
